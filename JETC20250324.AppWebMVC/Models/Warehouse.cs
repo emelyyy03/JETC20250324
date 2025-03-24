685d05a45350bcb1fb2018b745ebf726b26c8c08
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-03-24 09:41:57 -0600

    Configuracion para bodegas

--- JETC20250324.AppWebMVC/Models/Warehouse.cs
+++ JETC20250324.AppWebMVC/Models/Warehouse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JETC20250324.AppWebMVC.Models;
 
@@ -7,8 +8,11 @@ public partial class Warehouse
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [Display(Name = "Nombre")]
     public string WarehouseName { get; set; } = null!;
 
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
