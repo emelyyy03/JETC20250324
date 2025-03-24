d1fbc76a149b1d1b57f005fedbd3b3063aa96a37
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-03-24 09:33:51 -0600

    configuracion para Marcas

--- JETC20250324.AppWebMVC/Models/Brand.cs
+++ JETC20250324.AppWebMVC/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JETC20250324.AppWebMVC.Models;
 
@@ -7,8 +8,11 @@ public partial class Brand
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [Display(Name = "Nombre")]
     public string BrandName { get; set; } = null!;
 
+    [Display(Name = "País")]
     public string? Country { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
